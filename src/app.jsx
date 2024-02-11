1efea1963bf6837ae525094537e2c31d8ab35751
just-caarol <justcaarol@gmail.com>
2024-02-11 20:35:11 +0100

    add main react app.jsx file

---
+++ src/app.jsx
@@ -0,0 +1,13 @@
+import CssBaseline from '@mui/material/CssBaseline';
+import Grocery from './grocery/grocery';
+
+function App() {
+  return (
+    <>
+      <CssBaseline />
+      <Grocery />
+    </>
+  )
+}
+
+export default App
