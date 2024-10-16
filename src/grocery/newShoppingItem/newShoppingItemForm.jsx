5b098112b8c0f40f97058cbd031bd86665538e82
Karolina Straczek <justcaarol@gmail.com>
2024-10-16 12:20:58 +0200

    fix minor issues with padding and spacing

--- src/grocery/newShoppingItem/newShoppingItemForm.jsx
+++ src/grocery/newShoppingItem/newShoppingItemForm.jsx
@@ -1,32 +1,37 @@
 import { useState } from "react";
 
-import Button from '@mui/material/Button';
-import TextField from '@mui/material/TextField';
-import Box from '@mui/material/Box';
-import InputLabel from '@mui/material/InputLabel';
-import FormControl from '@mui/material/FormControl';
-import MenuItem from '@mui/material/MenuItem';
-import Select from '@mui/material/Select';
-import AddShoppingCartOutlinedIcon from '@mui/icons-material/AddShoppingCartOutlined';
+import AddShoppingCartOutlinedIcon from "@mui/icons-material/AddShoppingCartOutlined";
+import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
+import FormControl from "@mui/material/FormControl";
+import InputLabel from "@mui/material/InputLabel";
+import MenuItem from "@mui/material/MenuItem";
+import Select from "@mui/material/Select";
+import TextField from "@mui/material/TextField";
 
 export default function NewShoppingItemForm({ addItem }) {
-  const [formData, setFormData] = useState({ product: "", quantity: 0, unit: "", bought: false });
+  const [formData, setFormData] = useState({
+    product: "",
+    quantity: 0,
+    unit: "",
+    bought: false,
+  });
   const [productIsValid, setProductIsValid] = useState(false);
   const maxQuantity = 1000000;
 
-  const validateProduct = (product) => setProductIsValid(product.length !== 0)
+  const validateProduct = (product) => setProductIsValid(product.length !== 0);
 
   const handleChange = (evt) => {
     const { name, value } = evt.target;
-    const isQuantity = name === 'quantity';
+    const isQuantity = name === "quantity";
     const isProduct = name === "product";
 
     if (isProduct) validateProduct(value);
 
-    setFormData(currData => {
+    setFormData((currData) => {
       let finalValue;
       if (isQuantity) {
-        const parsedValue = parseInt(value.toLowerCase()).toString()
+        const parsedValue = parseInt(value.toLowerCase()).toString();
         finalValue = parsedValue > maxQuantity ? currData[name] : parsedValue;
       } else {
         finalValue = value.toLowerCase();
@@ -34,10 +39,10 @@ export default function NewShoppingItemForm({ addItem }) {
 
       return {
         ...currData,
-        [name]: finalValue
+        [name]: finalValue,
       };
     });
-  }
+  };
 
   const onSubmit = (e) => {
     e.preventDefault();
@@ -46,7 +51,7 @@ export default function NewShoppingItemForm({ addItem }) {
       setFormData({ product: "", quantity: 0, unit: "", bought: false });
       setProductIsValid(false);
     }
-  }
+  };
 
   return (
     <Box
@@ -55,14 +60,23 @@ export default function NewShoppingItemForm({ addItem }) {
       sx={{
         my: 1,
         mx: 2,
-        display: 'flex',
-        flexDirection: 'column',
-        alignItems: 'center',
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
         maxWidth: 750,
+        "& .MuiTextField-root": { m: 1 },
       }}
       onSubmit={onSubmit}
     >
-      <Box component="section" sx={{ display: 'flex' }}>
+      <Box
+        component="section"
+        sx={{
+          display: "flex",
+          flexWrap: "wrap",
+          justifyContent: "center",
+          "& .MuiFormControl-root": { m: "8px", display: "flex", flexGrow: 1 },
+        }}
+      >
         <TextField
           margin="normal"
           required
@@ -92,8 +106,13 @@ export default function NewShoppingItemForm({ addItem }) {
           inputProps={{ min: 0, max: maxQuantity }}
           sx={{ m: "auto 0.5rem" }}
         />
-        <FormControl required sx={{ m: "auto 0.5rem", minWidth: 222.4 }}>
-          <InputLabel error={!formData.unit} id="demo-simple-select-helper-label">Choose product unit</InputLabel>
+        <FormControl required>
+          <InputLabel
+            error={!formData.unit}
+            id="demo-simple-select-helper-label"
+          >
+            Choose product unit
+          </InputLabel>
           <Select
             labelId="demo-simple-select-helper-label"
             id="demo-simple-select-helper"
@@ -106,16 +125,36 @@ export default function NewShoppingItemForm({ addItem }) {
             <MenuItem value="">
               <em>Choose one unit:</em>
             </MenuItem>
-            <MenuItem name="unit" value={"piece"}>piece</MenuItem>
-            <MenuItem name="unit" value={"pieces"}>pieces</MenuItem>
-            <MenuItem name="unit" value={"pack"}>pack</MenuItem>
-            <MenuItem name="unit" value={"packages"}>packages</MenuItem>
-            <MenuItem name="unit" value={"bottle"}>bottle</MenuItem>
-            <MenuItem name="unit" value={"bottles"}>bottles</MenuItem>
-            <MenuItem name="unit" value={"kg"}>kilogram</MenuItem>
-            <MenuItem name="unit" value={"g"}>gram</MenuItem>
-            <MenuItem name="unit" value={"l"}>liter</MenuItem>
-            <MenuItem name="unit" value={"ml"}>mililiter</MenuItem>
+            <MenuItem name="unit" value={"piece"}>
+              piece
+            </MenuItem>
+            <MenuItem name="unit" value={"pieces"}>
+              pieces
+            </MenuItem>
+            <MenuItem name="unit" value={"pack"}>
+              pack
+            </MenuItem>
+            <MenuItem name="unit" value={"packages"}>
+              packages
+            </MenuItem>
+            <MenuItem name="unit" value={"bottle"}>
+              bottle
+            </MenuItem>
+            <MenuItem name="unit" value={"bottles"}>
+              bottles
+            </MenuItem>
+            <MenuItem name="unit" value={"kg"}>
+              kilogram
+            </MenuItem>
+            <MenuItem name="unit" value={"g"}>
+              gram
+            </MenuItem>
+            <MenuItem name="unit" value={"l"}>
+              liter
+            </MenuItem>
+            <MenuItem name="unit" value={"ml"}>
+              mililiter
+            </MenuItem>
           </Select>
         </FormControl>
       </Box>
@@ -125,12 +164,18 @@ export default function NewShoppingItemForm({ addItem }) {
         fullWidth
         variant="contained"
         color="warning"
-        sx={{ mt: 3, mb: 2, wordBreak: 'break-word', maxWidth: 715.72, textTransform: "none" }}
+        sx={{
+          mt: 3,
+          mb: 2,
+          wordBreak: "break-word",
+          maxWidth: 715.72,
+          textTransform: "none",
+        }}
         startIcon={<AddShoppingCartOutlinedIcon />}
         disabled={!productIsValid || !formData.unit || !formData.quantity}
       >
         {`Add to the list: ${formData.product} ${formData.quantity} ${formData.unit}`}
       </Button>
-    </Box >
+    </Box>
   );
 }
--- src/grocery/shoppingList/shoppingListItem.jsx
+++ src/grocery/shoppingList/shoppingListItem.jsx
@@ -1,13 +1,21 @@
-import HighlightOffOutlinedIcon from '@mui/icons-material/HighlightOffOutlined';
-import RadioButtonUncheckedIcon from '@mui/icons-material/RadioButtonUnchecked';
-import TaskAltIcon from '@mui/icons-material/TaskAlt';
-import { Checkbox, FormControlLabel, IconButton, ListItem, ListItemButton, Typography } from "@mui/material";
+import HighlightOffOutlinedIcon from "@mui/icons-material/HighlightOffOutlined";
+import RadioButtonUncheckedIcon from "@mui/icons-material/RadioButtonUnchecked";
+import TaskAltIcon from "@mui/icons-material/TaskAlt";
+import {
+  Checkbox,
+  FormControlLabel,
+  IconButton,
+  ListItem,
+  ListItemButton,
+  Typography,
+} from "@mui/material";
 
 export default function ShoppingListItem({ item, events }) {
   return (
     <ListItem
       sx={{
-        wordBreak: 'break-word',
+        wordBreak: "break-word",
+        "& .MuiListItemSecondaryAction-root": { marginRight: "1rem" },
       }}
       key={item.id}
       selected={item.bought}
@@ -15,35 +23,49 @@ export default function ShoppingListItem({ item, events }) {
         <IconButton
           edge="end"
           aria-label="delete"
-          onClick={() => events.removeProduct(item.id)}>
-          <HighlightOffOutlinedIcon sx={{
-            color: '#424242'
-          }} />
+          sx={{ paddingRight: "1rem" }}
+          onClick={() => events.removeProduct(item.id)}
+        >
+          <HighlightOffOutlinedIcon
+            sx={{
+              color: "#424242",
+            }}
+          />
         </IconButton>
       }
       disablePadding
     >
-
       <ListItemButton>
-        <FormControlLabel component="div" control={<Checkbox
-          checked={item.bought}
-          onChange={() => events.changePurchaseStatus(item.id)}
-          icon={<RadioButtonUncheckedIcon />}
-          checkedIcon={<TaskAltIcon />}
-          sx={{
-            color: '#424242',
-            '&.Mui-checked': {
-              color: '#ed6c02',
-            }
-          }}
-        />
-        } label={<Typography sx={{
-          fontFamily: 'Monospace',
-          letterSpacing: 3,
-          textDecoration: item.bought ? "line-through" : "none",
-        }}>{`${item.product} ${item.quantity} ${item.unit}`} </Typography>}
+        <FormControlLabel
+          component="div"
+          sx={{ paddingLeft: "1rem" }}
+          control={
+            <Checkbox
+              checked={item.bought}
+              onChange={() => events.changePurchaseStatus(item.id)}
+              icon={<RadioButtonUncheckedIcon />}
+              checkedIcon={<TaskAltIcon />}
+              sx={{
+                color: "#424242",
+                "&.Mui-checked": {
+                  color: "#ed6c02",
+                },
+              }}
+            />
+          }
+          label={
+            <Typography
+              sx={{
+                fontFamily: "Monospace",
+                letterSpacing: 3,
+                textDecoration: item.bought ? "line-through" : "none",
+              }}
+            >
+              {`${item.product} ${item.quantity} ${item.unit}`}
+            </Typography>
+          }
         />
       </ListItemButton>
     </ListItem>
-  )
+  );
 }
